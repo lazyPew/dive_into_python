adbb9cf2d7b60f889ebe2676f46d04f6ab6130be
meh-yashk <43238538+meh-yashk@users.noreply.github.com>
2020-09-07 22:53:15 +0300

    Update client.py

--- client.py
+++ client.py
@@ -14,19 +14,16 @@ class Client:
         try:
             self.connection.sendall(f"get {name}\n".encode("utf8"))
             response = self.connection.recv(1024).decode()
-            # if "ok\n" not in response or "\n\n" not in response:
-            #     raise ClientError
             metric_dict = {}
-            print(response)
             resp = str(response).split('\n')[1:-2]
+            
             if str(response).split('\n',1)[0] != "ok" or not response.endswith("\n\n"):
                 raise ClientError
             elif str(response) == "ok\n\n":
                 return metric_dict
-            print(resp)
+            
             for m in resp:
                 metrics = m.split(' ')
-                # print(m)
                 if len(metrics) == 3:
                     metric_key = metrics[0]
                     metric_value = float(metrics[1])
@@ -34,11 +31,10 @@ class Client:
                     metric_list = metric_dict.get(metric_key, [])
                     metric_list.append((metric_timestamp, metric_value))
                     metric_dict.update({metric_key: sorted(metric_list)})
-                    # print(type(metric_timestamp))
+                
                 else:
                     raise ClientError
-                # elif metrics not in [["b'ok"], [""], [""]]:
-                #     raise ClientError
+
             print(metric_dict)
             return metric_dict
 
@@ -46,15 +42,16 @@ class Client:
             raise ClientError
 
 
-    def put(self,name,numb,timestamp = None):
+    def put(self,key,value,timestamp = None):
         try:
             timestamp = timestamp or int(time.time())
-            self.connection.sendall(f"put {name} {numb} {timestamp}\n".encode("utf8"))
+            self.connection.sendall(f"put {key} {value} {timestamp}\n".encode("utf8"))
             response = self.connection.recv(1024)
             if response != b"ok\n\n":
                 raise ClientError
             else:
                 print(response.decode())
+        
         except Exception:
             raise ClientError
 
@@ -64,7 +61,5 @@ port = 8888
 timeout = 15
 client = Client(host,port,timeout)
 client.put("test_key", 0.5, timestamp=1150864247)
-client.put("palm.cpudfg", 0.4, timestamp=1150864245)
-client.put("palm.cpudfg", 0.6, timestamp=1150864242)
-client.put("palm.cpu123", 0.3, timestamp=1150864247)
 client.get("test_key")
+client.get("*")
